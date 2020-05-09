80e52790e9e014011fb013a0358056a13eb410ec
Hadrien Meyer <hadrien@MacBook-Pro-de-Hadrien-2.local>
2020-05-09 16:57:01 +0200

    add test file and test workflow

---
+++ findcoord/tests/test.py
@@ -0,0 +1,15 @@
+from findcoord.transform import findcoord
+import numpy as np
+
+
+def test_transformation():
+    TF = findcoord('Input.txt', 'Output.txt')
+    TF.calculate_coordinates()
+    assert TF.mesures_final_array_ == np.array(
+        [
+            [
+                124.39044127361605, 36.80539040824413], [
+                90.94469779584279, 39.44780410429261], [
+                    114.56211846783388, 44.50945864359023], [
+                        71.93591521394606, 40.41996830427888], [
+                            173.90661742788063, 34.465107806445175]])
