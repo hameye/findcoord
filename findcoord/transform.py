d4882745e163beed84b9fc15b5e7cb39b475f22c
Hadrien Meyer <meyerhadrien96@gmail.com>
2020-05-29 19:15:03 +0200

    Modify organization and language of type

--- findcoord/transform.py
+++ findcoord/transform.py
@@ -23,7 +23,7 @@
 #
 ###############################################################################
 __author__ = "Hadrien Meyer"
-__organization__ = "ENSG Nancy"
+__organization__ = "ENSG - Université de Lorraine"
 __email__ = "meyerhadrien96@gmail.com"
 __date__ = "March, 2020"
 
@@ -86,11 +86,11 @@ class transformation:
         See Mathematical definition of affine
         transformation and Projection (Homography) for more details. """
         self.repere_init_array_ = self.input_data.loc[
-            self.input_data['Type'] == 'Repere'].to_numpy()[:, 1:]
+            self.input_data['Type'].str.contains('ef')].to_numpy()[:, 1:]
         self.repere_final_array_ = self.output_data.loc[
-            self.output_data['Type'] == 'Repere'].to_numpy()[:, 1:]
+            self.output_data['Type'].str.contains('ef')].to_numpy()[:, 1:]
         self.mesures_init_array_ = self.input_data.loc[
-            self.input_data['Type'] == 'Mesure'].to_numpy()[:, 1:]
+            self.input_data['Type'].str.contains('easure')].to_numpy()[:, 1:]
 
         if type == 'Affine':
             self.transformation_ = tf.AffineTransform()
